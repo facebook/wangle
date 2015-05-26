fd4b1edcb33c90123791a7fa7631e23886f3fa5b
jsedgwick <jsedgwick@fb.com>
2015-05-26 15:28:31 -0700

    add initial examples

---
+++ example/proxy/Proxy.cpp
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2015 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gflags/gflags.h>
+
+#include <wangle/bootstrap/ClientBootstrap.h>
+#include <wangle/bootstrap/ServerBootstrap.h>
+#include <wangle/channel/AsyncSocketHandler.h>
+
+using namespace folly;
+using namespace folly::wangle;
+
+DEFINE_int32(port, 1080, "proxy server port");
+DEFINE_string(remote_host, "127.0.0.1", "remote host");
+DEFINE_int32(remote_port, 23, "remote port");
+
+class ProxyBackendHandler : public InboundBytesToBytesHandler {
+ public:
+  explicit ProxyBackendHandler(DefaultPipeline* frontendPipeline) :
+      frontendPipeline_(frontendPipeline) {}
+
+  void read(Context* ctx, IOBufQueue& q) override {
+    frontendPipeline_->write(q.move());
+  }
+
+  void readEOF(Context* ctx) override {
+    LOG(INFO) << "Connection closed by remote host";
+    frontendPipeline_->close();
+  }
+
+  void readException(Context* ctx, exception_wrapper e) override {
+    LOG(ERROR) << "Remote error: " << exceptionStr(e);
+    frontendPipeline_->close();
+  }
+
+ private:
+  DefaultPipeline* frontendPipeline_;
+};
+
+class ProxyBackendPipelineFactory : public PipelineFactory<DefaultPipeline> {
+ public:
+  explicit ProxyBackendPipelineFactory(DefaultPipeline* frontendPipeline) :
+      frontendPipeline_(frontendPipeline) {}
+
+  std::unique_ptr<DefaultPipeline, folly::DelayedDestruction::Destructor>
+  newPipeline(std::shared_ptr<AsyncSocket> sock) {
+
+    std::unique_ptr<DefaultPipeline, folly::DelayedDestruction::Destructor>
+      pipeline(new DefaultPipeline);
+    pipeline->addBack(AsyncSocketHandler(sock));
+    pipeline->addBack(ProxyBackendHandler(frontendPipeline_));
+    pipeline->finalize();
+
+    return std::move(pipeline);
+  }
+ private:
+  DefaultPipeline* frontendPipeline_;
+};
+
+class ProxyFrontendHandler : public BytesToBytesHandler {
+ public:
+  explicit ProxyFrontendHandler(SocketAddress remoteAddress) :
+      remoteAddress_(remoteAddress) {}
+
+  void read(Context* ctx, IOBufQueue& q) override {
+    backendPipeline_->write(q.move());
+  }
+
+  void readEOF(Context* ctx) override {
+    LOG(INFO) << "Connection closed by local host";
+    backendPipeline_->close().then([this, ctx](){
+      this->close(ctx);
+    });
+  }
+
+  void readException(Context* ctx, exception_wrapper e) override {
+    LOG(ERROR) << "Local error: " << exceptionStr(e);
+    backendPipeline_->close().then([this, ctx](){
+      this->close(ctx);
+    });
+  }
+
+  void transportActive(Context* ctx) override {
+    client_.pipelineFactory(
+        std::make_shared<ProxyBackendPipelineFactory>(
+            dynamic_cast<DefaultPipeline*>(ctx->getPipeline())));
+    client_.connect(remoteAddress_)
+      .then([this](DefaultPipeline* pipeline){
+        backendPipeline_ = pipeline;
+      })
+      .onError([this, ctx](const std::exception& e){
+        LOG(ERROR) << "Connect error: " << exceptionStr(e);
+        this->close(ctx);
+      });
+  }
+
+ private:
+  SocketAddress remoteAddress_;
+  ClientBootstrap<DefaultPipeline> client_;
+  DefaultPipeline* backendPipeline_;
+};
+
+class ProxyFrontendPipelineFactory : public PipelineFactory<DefaultPipeline> {
+ public:
+  explicit ProxyFrontendPipelineFactory(SocketAddress remoteAddress) :
+      remoteAddress_(remoteAddress) {}
+
+  std::unique_ptr<DefaultPipeline, folly::DelayedDestruction::Destructor>
+  newPipeline(std::shared_ptr<AsyncSocket> sock) {
+
+    std::unique_ptr<DefaultPipeline, folly::DelayedDestruction::Destructor>
+      pipeline(new DefaultPipeline);
+    pipeline->addBack(AsyncSocketHandler(sock));
+    pipeline->addBack(std::make_shared<ProxyFrontendHandler>(remoteAddress_));
+    pipeline->finalize();
+
+    return std::move(pipeline);
+  }
+ private:
+  SocketAddress remoteAddress_;
+};
+
+int main(int argc, char** argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  ServerBootstrap<DefaultPipeline> server;
+  server.childPipeline(std::make_shared<ProxyFrontendPipelineFactory>(
+      SocketAddress(FLAGS_remote_host, FLAGS_remote_port)));
+  server.bind(FLAGS_port);
+  server.waitForStop();
+
+  return 0;
+}
---
+++ example/telnet/TelnetClient.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <iostream>
+#include <gflags/gflags.h>
+
+#include <wangle/bootstrap/ClientBootstrap.h>
+#include <wangle/channel/AsyncSocketHandler.h>
+#include <wangle/channel/EventBaseHandler.h>
+#include <wangle/codec/LineBasedFrameDecoder.h>
+#include <wangle/codec/StringCodec.h>
+
+using namespace folly;
+using namespace folly::wangle;
+
+DEFINE_int32(port, 23, "test telnet server port");
+DEFINE_string(host, "::1", "test telnet server address");
+
+typedef Pipeline<folly::IOBufQueue&, std::string> TelnetPipeline;
+
+class TelnetHandler : public HandlerAdapter<std::string> {
+ public:
+  virtual void read(Context* ctx, std::string msg) override {
+    std::cout << msg;
+  }
+  virtual void readException(Context* ctx, exception_wrapper e) override {
+    std::cout << exceptionStr(e) << std::endl;
+    close(ctx);
+  }
+  virtual void readEOF(Context* ctx) override {
+    std::cout << "EOF received :(" << std::endl;
+    close(ctx);
+  }
+};
+
+class TelnetPipelineFactory : public PipelineFactory<TelnetPipeline> {
+ public:
+  std::unique_ptr<TelnetPipeline, folly::DelayedDestruction::Destructor>
+  newPipeline(std::shared_ptr<AsyncSocket> sock) {
+    auto pipeline = folly::make_unique<TelnetPipeline, folly::DelayedDestruction::Destructor>();
+    pipeline->addBack(AsyncSocketHandler(sock));
+    pipeline->addBack(EventBaseHandler()); // ensure we can write from any thread
+    pipeline->addBack(LineBasedFrameDecoder(8192, false));
+    pipeline->addBack(StringCodec());
+    pipeline->addBack(TelnetHandler());
+    pipeline->finalize();
+
+    return pipeline;
+  }
+};
+
+int main(int argc, char** argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  ClientBootstrap<TelnetPipeline> client;
+  client.group(std::make_shared<folly::wangle::IOThreadPoolExecutor>(1));
+  client.pipelineFactory(std::make_shared<TelnetPipelineFactory>());
+  auto pipeline = client.connect(SocketAddress(FLAGS_host,FLAGS_port)).get();
+
+  try {
+    while (true) {
+      std::string line;
+      std::getline(std::cin, line);
+      if (line == "") {
+        break;
+      }
+
+      // Sync write will throw exception if server goes away
+      pipeline->write(line + "\r\n").get();
+      if (line == "bye") {
+        pipeline->close();
+        break;
+      }
+    }
+  } catch(const std::exception& e) {
+    std::cout << exceptionStr(e) << std::endl;
+  }
+
+  return 0;
+}
