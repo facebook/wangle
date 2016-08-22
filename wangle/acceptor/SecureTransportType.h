0c1f20b8b597b55428561e958f887768074970a2
Kyle Nekritz <knekritz@fb.com>
2016-08-22 13:25:12 -0700

    Add namespace to SecureTransportType.
    Summary:
    Previously this was un-namespaced, which wasn't great.
    Relanding D3711046 a second time, with backwards compatibility with the un-namespaced version.
    
    Reviewed By: Orvid
    
    Differential Revision: D3752103
    
    fbshipit-source-id: d2a01b122bf9dfa8f5c7560916c06a3dbe87033e

--- wangle/acceptor/SecureTransportType.h
+++ wangle/acceptor/SecureTransportType.h
@@ -1,4 +1,15 @@
+/*
+ *  Copyright (c) 2016, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
 #pragma once
+
+namespace wangle {
 
 /**
  * An enum representing different kinds
@@ -9,3 +20,9 @@ enum class SecureTransportType {
   TLS,  // Transport is based on TLS
   ZERO, // Transport is based on zero protocol
 };
+
+}
+
+// This enum used to be un-namespaced. This is here for temporary backwards
+// compatibility with old uses of this enum.
+using wangle::SecureTransportType;
