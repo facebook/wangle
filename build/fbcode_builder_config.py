91d9fb1719cb32d03296763e4073abd77dcd7a86
Adam Simpkins <simpkins@fb.com>
2018-04-10 12:15:54 -0700

    add fbcode_builder configs
    Summary: Add configuration files to tell fbcode_builder how to build and test wangle.
    
    Reviewed By: akushner
    
    Differential Revision: D7529697
    
    fbshipit-source-id: caa4e634984015469c441479621efc196433554c

---
+++ build/fbcode_builder_config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+'fbcode_builder steps to build & test Wangle'
+
+import specs.gmock as gmock
+import specs.folly as folly
+
+from shell_quoting import ShellQuoted
+
+
+def fbcode_builder_spec(builder):
+    builder.add_option(
+        'wangle/_build:cmake_defines',
+        {
+            'BUILD_SHARED_LIBS': 'OFF',
+            'BUILD_TESTS': 'ON',
+        }
+    )
+    return {
+        'depends_on': [gmock, folly],
+        'steps': [
+            builder.fb_github_cmake_install('wangle/_build', '../wangle'),
+            builder.step(
+                'Run wangle tests', [
+                    builder.run(
+                        ShellQuoted('ctest --output-on-failure -j {n}')
+                        .format(n=builder.option('make_parallelism'), )
+                    )
+                ]
+            ),
+        ]
+    }
+
+
+config = {
+    'github_project': 'facebook/wangle',
+    'fbcode_builder_spec': fbcode_builder_spec,
+}
