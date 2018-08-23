776c65d1647fc3cef0e44cc4a8018eb9ff476b0f
Ajanthan Asogamoorthy <aasogamo@fb.com>
2018-08-22 18:19:46 -0700

    add fizz context creation to wangle
    Summary: Add basic utility function to fizz + wangle in order to create a fizz context from an AcceptorConfig
    
    Reviewed By: knekritz
    
    Differential Revision: D9200293
    
    fbshipit-source-id: 12367bb73dfcebca8079971ea28c3e36e04c9580

---
+++ wangle/acceptor/FizzConfigUtil.cpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <wangle/acceptor/FizzConfigUtil.h>
+
+#include <fizz/protocol/DefaultCertificateVerifier.h>
+#include <fizz/util/FizzUtil.h>
+#include <folly/Format.h>
+
+using fizz::CertUtils;
+using fizz::DefaultCertificateVerifier;
+using fizz::ProtocolVersion;
+using fizz::PskKeyExchangeMode;
+using fizz::VerificationContext;
+using fizz::server::ClientAuthMode;
+using fizz::FizzUtil;
+
+namespace wangle {
+
+std::shared_ptr<fizz::server::FizzServerContext>
+FizzConfigUtil::createFizzContext(
+    const wangle::ServerSocketConfig& config) {
+  if (config.sslContextConfigs.empty()) {
+    return nullptr;
+  }
+
+  auto certMgr = std::make_unique<fizz::server::CertManager>();
+  auto loadedCert = false;
+  for (const auto& sslConfig : config.sslContextConfigs) {
+    for (const auto& cert : sslConfig.certificates) {
+      try {
+        auto x509Chain = FizzUtil::readChainFile(cert.certPath);
+        auto pkey = FizzUtil::readPrivateKey(cert.keyPath, cert.passwordPath);
+        auto selfCert =
+            CertUtils::makeSelfCert(std::move(x509Chain), std::move(pkey));
+        certMgr->addCert(std::move(selfCert), sslConfig.isDefault);
+        loadedCert = true;
+      } catch (const std::runtime_error& ex) {
+        auto msg = folly::sformat(
+            "Failed to load cert or key at key path {}, cert path {}",
+            cert.keyPath,
+            cert.certPath);
+        if (config.strictSSL) {
+          throw std::runtime_error(ex.what() + msg);
+        } else {
+          LOG(ERROR) << msg << ex.what();
+        }
+      }
+    }
+  }
+  if (!loadedCert) {
+    return nullptr;
+  }
+
+  auto ctx = std::make_shared<fizz::server::FizzServerContext>();
+  ctx->setSupportedVersions({ProtocolVersion::tls_1_3,
+                             ProtocolVersion::tls_1_3_28,
+                             ProtocolVersion::tls_1_3_26});
+  ctx->setVersionFallbackEnabled(true);
+
+  // Fizz does not yet support randomized next protocols so we use the highest
+  // weighted list on the first context.
+  const auto& list = config.sslContextConfigs.front().nextProtocols;
+  if (!list.empty()) {
+    ctx->setSupportedAlpns(FizzUtil::getAlpnsFromNpnList(list));
+  }
+  ctx->setCertManager(std::move(certMgr));
+
+  auto verify = config.sslContextConfigs.front().clientVerification;
+  switch (verify) {
+    case folly::SSLContext::SSLVerifyPeerEnum::VERIFY_REQ_CLIENT_CERT:
+      ctx->setClientAuthMode(ClientAuthMode::Required);
+      break;
+    case folly::SSLContext::SSLVerifyPeerEnum::VERIFY:
+      ctx->setClientAuthMode(ClientAuthMode::Optional);
+      break;
+    default:
+      ctx->setClientAuthMode(ClientAuthMode::None);
+  }
+
+  auto caFile = config.sslContextConfigs.front().clientCAFile;
+  if (!caFile.empty()) {
+    auto verifier = DefaultCertificateVerifier::createFromCAFile(
+        VerificationContext::Server, caFile);
+    ctx->setClientCertVerifier(std::move(verifier));
+  }
+
+  return ctx;
+}
+
+}
